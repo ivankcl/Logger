d4d5d3594fd5620418933309b5dfbaf0266b4853
ckwong6 <72502827+ivankcl@users.noreply.github.com>
2024-03-17 14:52:57 +0800

    Create background thread for async writing

--- LogComponent.py
+++ LogComponent.py
@@ -1,21 +1,48 @@
 from datetime import datetime
+
+import threading
 import os
+import time
 
 class LogComponent():
 
-    log_folder = 'Log'
+    log_folder_path = 'Log'
 
     def __init__(self):
-        if not os.path.exists(self.log_folder):
-            os.makedirs(self.folder)
+        self.message_queue = []
+        self.running = True
+        if not os.path.exists(self.log_folder_path):
+            os.makedirs(self.log_folder_path)
 
+        self.background_thread = threading.Thread(target=self._background_writing)
+        self.background_thread.start()
 
     def write(self, message: str):
+        self.message_queue.append(message)
+
+    def stop(self, wait_for_outstanding_log = True):
+        self.running = False
+        self.background_thread.join()
+
+
+    def _background_writing(self):
+        while (self.running):
+            if (self.message_queue):
+                message = self.message_queue.pop(0)
+                self._write_to_file(message)
+            else:
+                time.sleep(0.1)
+
+    def _write_to_file(self, message: str):
         log_name = f'log_{datetime.now().strftime("%Y%m%d")}.txt'
-        log_path = os.path.join(self.log_folder, log_name)
+        log_path = os.path.join(self.log_folder_path, log_name)
         with open(log_path, "a") as file:
-            file.write(message)
+            file.write(message + '\n')
         
 if __name__ == '__main__':
     logComponent = LogComponent()
-    logComponent.write('Hello World' + '\n')
+    for _ in range(10):
+        logComponent.write('Hello World')
+    
+    time.sleep(5)
+    logComponent.stop()
