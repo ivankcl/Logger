94cfb1d468356324d63fcb4c139a37cdbe5dc0a9
ckwong6 <72502827+ivankcl@users.noreply.github.com>
2024-03-17 15:09:54 +0800

    Use python in-built queue structure to handle sync issue

--- LogComponent.py
+++ LogComponent.py
@@ -3,13 +3,14 @@ from datetime import datetime
 import threading
 import os
 import time
+import queue
 
 class LogComponent():
 
     log_folder_path = 'Log'
 
     def __init__(self):
-        self.message_queue = []
+        self.message_queue = queue.Queue()
         self.running = True
         if not os.path.exists(self.log_folder_path):
             os.makedirs(self.log_folder_path)
@@ -18,11 +19,11 @@ class LogComponent():
         self.background_thread.start()
 
     def write(self, message: str):
-        self.message_queue.append(message)
+        self.message_queue.put(message)
 
     def stop(self, wait_for_outstanding_log: bool = True):
         if (wait_for_outstanding_log):
-            while (self.message_queue):
+            while (not self.message_queue.empty()):
                 time.sleep(0.1)
 
         self.running = False
@@ -30,11 +31,12 @@ class LogComponent():
 
     def _background_writing(self):
         while (self.running):
-            if (self.message_queue):
-                message = self.message_queue.pop(0)
-                self._write_to_file(message)
-            else:
-                time.sleep(0.1)
+            if (not self.message_queue.empty()):
+                try: 
+                    message = self.message_queue.get(timeout=0.1)
+                    self._write_to_file(message)
+                except queue.Empty:
+                    pass # Do nothing if the queue is empty
 
     def _write_to_file(self, message: str):
         log_name = f'log_{datetime.now().strftime("%Y%m%d")}.txt'
