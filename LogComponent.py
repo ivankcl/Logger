6018f905315a52cdee59dcf863c82b3c06481450
ckwong6 <72502827+ivankcl@users.noreply.github.com>
2024-03-17 16:54:12 +0800

    Add try catch blocks to ensure the exceptions are contained

--- LogComponent.py
+++ LogComponent.py
@@ -1,12 +1,11 @@
-from datetime import datetime
-
 import threading
 import os
 import time
 import queue
 
-class LogComponent():
+from datetime import datetime
 
+class LogComponent():
 
     def __init__(self, log_folder_path: str = 'Log'):
 
@@ -14,28 +13,45 @@ class LogComponent():
             os.makedirs(log_folder_path)
 
         self.log_folder_path = log_folder_path
-
         self.message_queue = queue.Queue()
-        self.stop_event = threading.Event()
-        self.background_thread = threading.Thread(target=self._background_writing)
-        self.background_thread.start()
+
+        try:
+            # To write asynchronously without blocking the main application, a new thread is created
+            self.stop_event = threading.Event()
+            self.background_thread = threading.Thread(target=self._background_writing)
+            self.background_thread.start()
+        except Exception as e:
+            print('The thread could not be created: ', e)
 
     def write(self, message: str):
-        self.message_queue.put(message)
+        try:
+            # put() here is virtually atomic
+            self.message_queue.put(message)
+        except Exception as e:
+            print('The message could not be logged: ', e)
 
     def stop(self, wait_for_outstanding_log: bool = True):
+
+        try:
         
-        if (not self.stop_event.is_set()):
-            if (wait_for_outstanding_log):
-                while (not self.message_queue.empty()):
-                    time.sleep(0.1)
+            if (not self.stop_event.is_set()):
+                if (wait_for_outstanding_log):
 
-            self.stop_event.set()
-            self.background_thread.join()
+                    # Write until empty
+                    while (not self.message_queue.empty()):
+                        time.sleep(0.1)
 
-        return list(self.message_queue.queue)
+                self.stop_event.set()
+                self.background_thread.join()
+
+            # Return the list of messages that is not written to the log
+            return list(self.message_queue.queue)
+        except Exception as e:
+            print('The logger could not be stopped: ', e)
 
     def _background_writing(self):
+
+        # Background thread's main loop, asynchronous writing
         while (not self.stop_event.is_set()):
             if (not self.message_queue.empty()):
                 try: 
@@ -50,12 +66,11 @@ class LogComponent():
         return log_path
 
     def _write_to_file(self, message: str):
+
+        # Since Python could not interrupt the OS during the writing of a message,
+        # we could not the message writing cross midnight.
+        # The code here would only start a new date when the message starts writing on a new day
+
         log_path = self._get_file_path()
         with open(log_path, "a") as file:
             file.write(message)
-        
-if __name__ == '__main__':
-    logComponent = LogComponent()
-    for _ in range(10):
-        logComponent.write('Hello World')
-    logComponent.stop()
